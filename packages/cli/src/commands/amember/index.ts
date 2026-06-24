41d9e921e84fb5f7424cb05186201d9018549760
Justin Kumpe <jakumpe@kumpes.com>
2026-06-23 19:31:37 -0500

    [bug/#22] Fixed

--- packages/cli/src/commands/amember/index.ts
+++ packages/cli/src/commands/amember/index.ts
@@ -1,3 +1,4 @@
+import { noop } from '@silverhand/essentials';
 import type { CommandModule } from 'yargs';
 
 import sync from './sync.js';
@@ -6,6 +7,7 @@ const amember: CommandModule = {
   command: 'amember <command>',
   describe: 'aMember integration commands',
   builder: (yargs) => yargs.command(sync).demandCommand(1),
+  handler: noop,
 };
 
 export default amember;
--- packages/plugin-amember-sync/tsup.config.ts
+++ packages/plugin-amember-sync/tsup.config.ts
@@ -2,4 +2,7 @@ import { defineConfig } from 'tsup';
 
 import { defaultConfig } from '../../tsup.shared.config.js';
 
-export default defineConfig(defaultConfig);
+export default defineConfig({
+  ...defaultConfig,
+  dts: true,
+});
